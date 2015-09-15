337f11c467c839e398e3c82a74b85e0e350aa69a
itgsod-filip-forsberg <filip.forsberg@elev.it-gymnasiet.se>
2015-09-15 09:31:26 +0200

    Filip & Kirre ticket calc masters

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -16,5 +16,18 @@ def example(argument,anotherparmeter):
 
 
 
+def age_calc(age):
+    if age > 0  and age <= 17:
+        return "10 SEK"
+    elif age > 17 and age < 65:
+        return "20 SEK"
+    elif age >= 65:
+        return "15 SEK"
+    """
+
+    :param age: defines age
+    :return: Returns the cost of a ticket in a string
+    """
+
 
 
